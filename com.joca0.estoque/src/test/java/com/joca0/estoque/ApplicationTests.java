0a61853e90f25c042107234e46760e8198ba0716
Joca0 <joaosalu22@outlook.com>
2025-09-09 16:58:26 -0300

    springinitializer

---
+++ com.joca0.estoque/src/test/java/com/joca0/estoque/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joca0.estoque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
